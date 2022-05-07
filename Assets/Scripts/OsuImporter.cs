5b0a7c3da5f5dd8463ad30958df98f08c9202e4f
Fabian Lindgren <lindgrenfabian@gmail.com>
2022-05-07 11:31:26 +0200

    Read osu files as text assets

---
+++ Assets/Scripts/OsuImporter.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.IO;
+using UnityEditor.AssetImporters;
+
+[ScriptedImporter( 1, "osu" )]
+public class OsuImporter : ScriptedImporter {
+    public override void OnImportAsset( AssetImportContext ctx ) {
+        TextAsset subAsset = new TextAsset( File.ReadAllText( ctx.assetPath ) );
+        ctx.AddObjectToAsset( "text", subAsset );
+        ctx.SetMainObject( subAsset );
+    }
+}
